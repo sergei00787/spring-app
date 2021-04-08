1012ad1cb64d9ecb7a911dcbc55ebcb1cc22fe21
sergei00787 <sergei007_87@mail.ru>
2021-04-08 16:15:35 +0700

    Add Put method to EventHistory controller.

--- src/main/java/com/jbond/springlesson/controller/EventHistoryController.java
+++ src/main/java/com/jbond/springlesson/controller/EventHistoryController.java
@@ -12,8 +12,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import javax.print.attribute.standard.Media;
 import javax.swing.text.html.Option;
 import java.util.Collection;
+import java.util.Objects;
 import java.util.Optional;
 
 @RestController
@@ -32,7 +34,8 @@ public class EventHistoryController {
     @GetMapping()
     public Collection<EventHistory> getAllEventHistory() throws NotFoundException {
         Collection<EventHistory> optionalEventHistory = (Collection<EventHistory>) eventHistoryRepository.findAll();
-        if (optionalEventHistory.isEmpty()) throw new NotFoundException("Event History not found");;
+        if (optionalEventHistory.isEmpty()) throw new NotFoundException("Event History not found");
+        ;
         return optionalEventHistory;
     }
 
@@ -46,21 +49,31 @@ public class EventHistoryController {
         throw new NotFoundException("Event History with ID=" + id + " not found");
     }
 
-//    {
+    //    {
 //        "id": 999,
 //        "time": "08.01.2020T08:00:00",
 //        "timeCreate": "08.01.2020T08:00:00",
 //        "eventDescription": "BlaBalBla",
 //        "vehicle": {
-//        "id": 1
-//        },
+//          "id": 1
+//          },
 //        "eventType": {
-//        "id": 2
-//        }
+//          "id": 2
+//          }
 //    }
-    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
-    public EventHistory postEventHistory(@RequestBody EventHistory eventHistory){
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public EventHistory postEventHistory(@RequestBody EventHistory eventHistory) {
         return eventHistoryRepository.save(eventHistory);
+    }
+
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public EventHistory updateEventHistory(@RequestBody EventHistory eventHistory) {
+        EventHistory evHist = eventHistoryRepository.findById(eventHistory.getId()).get();
+//        Objects. .isNull(evHist) throw new NotFoundException("EventHistory not found);
+        evHist.setTime(eventHistory.getTime());
+        evHist.setTimeCreate(eventHistory.getTimeCreate());
+        return eventHistoryRepository.save(evHist);
     }
 
 
