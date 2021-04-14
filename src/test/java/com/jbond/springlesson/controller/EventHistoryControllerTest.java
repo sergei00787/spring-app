b0c04d7d1bf96721a5f4861dd81c2d0bc22460b9
sergei00787 <sergei007_87@mail.ru>
2021-04-14 23:19:28 +0700

    Add test Update and Delete EventHistoryController.

--- src/test/java/com/jbond/springlesson/controller/EventHistoryControllerTest.java
+++ src/test/java/com/jbond/springlesson/controller/EventHistoryControllerTest.java
@@ -1,16 +1,25 @@
 package com.jbond.springlesson.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jbond.springlesson.domain.EventHistory;
+import com.jbond.springlesson.domain.EventType;
+import com.jbond.springlesson.domain.Vehicle;
+import com.jbond.springlesson.repo.EventHistoryRepository;
+import com.jbond.springlesson.repo.EventTypeRepository;
+import com.jbond.springlesson.repo.VehicleRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDateTime;
+
 import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -18,10 +27,18 @@ class EventHistoryControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+    @Autowired
+    private EventHistoryRepository eventHistoryRepository;
+    @Autowired
+    private EventTypeRepository eventTypeRepository;
+    @Autowired
+    private VehicleRepository vehicleRepository;
 
     @Test
-    void getAllEventHistory() throws Exception {
-        this.mockMvc.perform(get("/event_histories"))
+    public void getAllEventHistory() throws Exception {
+        mockMvc.perform(get("/event_histories"))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content()
@@ -29,17 +46,39 @@ class EventHistoryControllerTest {
     }
 
     @Test
-    void getEventHistoryById() throws Exception {
-        this.mockMvc.perform(get("/event_histories/1"))
+    public void getEventHistoryById() throws Exception {
+        mockMvc.perform(get("/event_histories/1"))
                 .andDo(print())
                 .andExpect(status()
                         .isNotFound());
-        this.mockMvc.perform(get("/event_histories/9"))
+        mockMvc.perform(get("/event_histories/9"))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content()
                         .string(containsString("DESCR")));
     }
 
+    @Test
+    public void updateEventHistoryById() throws Exception {
+        Vehicle vehicle = new Vehicle("VehicleUpdate");
+        vehicleRepository.save(vehicle);
+        EventType eventType = new EventType("eventTypeUpdated");
+        eventTypeRepository.save(eventType);
+
+        EventHistory eventHistory = new EventHistory(3L, LocalDateTime.now().minusMinutes(20), LocalDateTime.now(), "UPDATE_TEST", vehicle, eventType);
+
+        this.mockMvc.perform((put("/event_histories")
+                .content(objectMapper.writeValueAsString(eventHistory))
+                .contentType(MediaType.APPLICATION_JSON)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").isNumber())
+                .andExpect(jsonPath("$.eventDescription").value("UPDATE_TEST"));
+    }
+
+    @Test
+    public void deleteEventHistoryById() throws Exception {
+        mockMvc.perform(delete("/event_histories/6"))
+                .andExpect(status().isOk());
+    }
 
 }
